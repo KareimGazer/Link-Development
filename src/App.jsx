31802c0117da10316918a30a368773ae03eeda09
Kareim Tarek <kareimgazar1990@gmail.com>
2024-12-26 11:56:36 +0200

    fix(fonts and styles): general styling and fonts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/services'
+import News from './components/News'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app">
+      <div className="heading-container">
+        <div className="animation-container">
+            <div className="bg-animation"></div>
+        </div>
+        <Navbar />
+        <Hero />
+      </div>
+
+      <Services />
+      <News />
+      <Footer />
+    </div>
   )
 }
 
