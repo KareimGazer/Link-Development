69ceb36baee3ba607723cb4477379067262371c6
Kareim Tarek <kareimgazar1990@gmail.com>
2024-12-26 11:47:30 +0200

    feat(hero): to be integrated with axios

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,35 @@
+import { SiGoogledisplayandvideo360 } from "react-icons/si";
+import './Hero.css';
+
+const Hero = () => {
+    return (
+        <main className="grid-container grid-container--hero flow">
+            <div className="indicators">
+                <button className='indicator active'><span className="sr-only active">marking</span></button>
+                <button className='indicator'><span className="sr-only">crafty</span></button>
+                <button className='indicator'><span className="sr-only">outsource</span></button>
+            </div>
+            <article className="hero-details flow">
+            <header className="flow">
+                <p className="hero__subtitle">marketing</p>
+                <h1 className="hero__title">25 Years of Business Growth</h1>
+            </header>
+                <p className="hero__desc">
+                    Link Development, the global technology solutions provider and an A15 company, unveiled today that it has recently marked its Silver Jubilee anniversary.
+                </p>
+
+                <div className='hero__cta indicators'>
+                    <a href="#" className="info-btn cta-btn">Find out more</a>
+                    <a href="#" className="video-btn cta-btn"><SiGoogledisplayandvideo360 /></a>
+                </div>
+            </article>
+
+            <picture>
+                <source srcSet="/people/boy.webp" type="image/webp"/>
+                <img src="/people/boy.png" alt="Douglas Hurley"/>
+            </picture>
+        </main>
+    );
+}
+
+export default Hero;
