ddf55d93815c69a1424aa906a844baaa4770ccf1
Kareim Tarek <kareimgazar1990@gmail.com>
2024-12-30 07:29:47 +0200

    fix(news) completed card implementation - social links section

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 import { getNews, getNewsCategories } from "../../utils"
+import { SlCalender } from "react-icons/sl";
+import { FaHeart, FaRegHeart,  } from "react-icons/fa";
+import { CiShare2 } from "react-icons/ci";
 
 import './News.css'
 
@@ -40,8 +43,8 @@ const News = () => {
             </div>
             <div className="news__filters">
                 {categories.map((category) => <button key={category.id}
-                    onClick={() => { setSelectedCategory(category.id); console.log(category.id) }}
-                    className="filter-btn"
+                    onClick={() => { setSelectedCategory(category.id)}}
+                    className={'filter-btn ' + (selectedCategory === category.id ? 'filter-btn-active' : '')}
                 >{category.name}</button>)}
             </div>
             <div className="news__grid">
@@ -53,12 +56,16 @@ const News = () => {
                                 <img className="article__avatar" src={newsItem.urlToImage} />
                             </div>
                             <div className="article-body">
-                                <h3><a href="https://linkdevelopment.com/" className="article-desc">{newsItem.title}</a></h3>
-                                <p className="article-date">{newsItem.publishedDate}</p>
+                                <h3  className="article-desc"><a href="https://linkdevelopment.com/">{newsItem.title}</a></h3>
+                                <div className="article-date">
+                                    <SlCalender className="article-icon" />
+                                    <div>{newsItem.publishedDate}</div>
+                                </div>
                                 <div className="cols-2">
                                     <p className="article-category">
                                         {categories.filter(category => category.id === newsItem.categoryID)[0]?.name}
                                     </p>
+                                    <SocialLinks />
                                 </div>
                             </div>
                         </div>
@@ -68,6 +75,24 @@ const News = () => {
             <button className="more-news-btn">View All News</button>
         </div>
     )
+}
+
+const SocialLinks = () => {
+    const [liked, setLiked] = useState(false)
+    const [shared, setShared] = useState(false)
+    return (
+        <div className="social-links">
+            <div onClick={() => setLiked(!liked)}>{liked ? <FaHeart /> : <FaRegHeart />}</div>
+            <div>
+                <CiShare2 onClick={() => setShared(!shared)} />
+                {shared && <div className="share-links">
+                    <img src="https://store-images.s-microsoft.com/image/apps.37935.9007199266245907.b029bd80-381a-4869-854f-bac6f359c5c9.91f8693c-c75b-4050-a796-63e1314d18c9?h=210" alt="facebook" />
+                    <img src="https://yt3.googleusercontent.com/ecGSwfk4BtzH_-0VPoZE9zw1dKdOyHvvacJaJ8Is0N0vhh2MIBSkWAy1yPf3MSoiZzLL0C3uF6Q=s900-c-k-c0x00ffffff-no-rj" alt="twitter" />
+                    <img src="https://i.pinimg.com/originals/f1/ba/a8/f1baa8c8c56cb9840b44f39628915e37.jpg" alt="gmail" />
+                </div>}
+            </div>
+        </div>
+    );
 }
 
 export default News
